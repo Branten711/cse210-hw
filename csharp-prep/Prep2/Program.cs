c7ed8d80b4ec51994244b641ec7104226eb51467
Branten711 <jud23009@byui.edu>
2026-01-15 13:56:45 -0700

    Finished

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter the grade percentage %");
+        string grd = Console.ReadLine();
+
+        
+        int grade = int.Parse(grd);
+        string letter;
+        if (grade >= 90)
+        {
+            letter = "A"; 
+        }
+        else if (grade >= 80)
+        {
+            letter = "B"; 
+        }
+        else if (grade >= 70)
+        {
+            letter = "C"; 
+        }
+        else if (grade >= 60)
+        {
+            letter = "D"; 
+        }
+        else if (grade <= 60)
+        {
+            letter = "F"; 
+        }
+        else
+        {
+            letter = "An error has occured";
+        }
+
+        Console.WriteLine($"The letter grade is {letter}.");
+        if (grade < 70)
+        {
+            Console.WriteLine("Sorry you did not pass. Keep going you got this!");
+        }
+        else if (grade > 70)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+        
+   
     }
+
 }
