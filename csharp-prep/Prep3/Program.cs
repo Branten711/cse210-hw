4c1a1b664be4f8865299cfaca610725c0d73505d
Branten711 <jud23009@byui.edu>
2026-01-21 15:15:17 -0700

    Implement guessing game and user input handling in Prep3 and Prep4; add user prompts and calculations in Prep5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 100);
+        // Console.Write("What is the magic number: ");
+        // string n = Console.ReadLine();
+        // int magic = int.Parse(n);
+        int guess = -1;
+        int run = 0;
+        while (magic != guess)
+        {
+            Console.Write("What is your guess: ");
+            string gus = Console.ReadLine();
+            guess = int.Parse(gus);
+
+
+            if (guess > magic)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magic)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            run++;
+
+        }
+        Console.WriteLine($"It took you {run} guesses.");
     }
 }
