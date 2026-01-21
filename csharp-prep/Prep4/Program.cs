b010fceed6ff8a48ff451107e7362e2ad5526d99
Branten711 <jud23009@byui.edu>
2026-01-21 15:17:29 -0700

    finished

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,38 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number = 1;
+        int run = 0;
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            string i = Console.ReadLine();
+            number = int.Parse(i);
+            if (number != 0)
+            {
+              run++;
+              numbers.Add(number); 
+            }
+            
+        }
+        int total = 0;
+        int large = 0;
+        foreach (int num in numbers)
+        {
+            if (num > large)
+            {
+                large = num;
+            }
+            total += num;
+        }
+        double average = total / run;
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {large}");
+
     }
 }
