c935b18b6b5cd35eae248d9708540baaf0c1068b
Branten711 <jud23009@byui.edu>
2026-02-11 12:08:40 -0700

    finished

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,15 @@
 using System;
-
+using System.Diagnostics.Contracts;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine("Welcome to the Journal Program!");
+        Journal run = new Journal();
+        run.Run();
+        Console.WriteLine("Thank you for writing in your journal");
     }
 }
